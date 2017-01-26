21aad1d4c2dd4de5304594ed4ff0224b9c17d074
rubygeek <nola@rubygeek.com>
2017-01-25 21:36:15 -0600

    basic-timer

--- pomadoro.js
+++ pomadoro.js
@@ -1,6 +1,32 @@
 
-var timer = function(minutes) {
-//  console.log("Setting Timer for " + minutes + " minutes.");
-} 
+var bell = new Audio("./ship-bell.mp3");
 
-timer(25);
+var timer = null;
+
+var pomadoro_count = 0;
+var break_count = 0;
+
+var startTimer = function(minutes) {
+  timer = setTimeout(endTimer, minutes * 60 * 1000);
+}
+
+var endTimer = function() {
+  console.log("Ring the bell and stop timer");
+  bell.play();
+}
+
+var pomadoro = function() {
+  console.log("Doing a pomadoro");
+  startTimer(25);
+}
+
+var breaktime = function() {
+  console.log("Enjoy your break!");
+  startTimer(5);
+}
+
+var stop = function() {
+  clearTimeout(timer);
+  console.log("Timer has been stopped");
+}
+
