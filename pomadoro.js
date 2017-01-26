c2e0dd611cd8012a20f8b617ce62b6f79f8b4efb
rubygeek <nola@rubygeek.com>
2017-01-25 21:55:42 -0600

    added some display

--- pomadoro.js
+++ pomadoro.js
@@ -4,7 +4,7 @@ var bell = new Audio("./ship-bell.mp3");
 var timer = null;
 
 var pomadoro_count = 0;
-var break_count = 0;
+var stop_count = 0;
 
 var startTimer = function(minutes) {
   timer = setTimeout(endTimer, minutes * 60 * 1000);
@@ -13,11 +13,13 @@ var startTimer = function(minutes) {
 var endTimer = function() {
   console.log("Ring the bell and stop timer");
   bell.play();
+  pomadoro_count++;
 }
 
 var pomadoro = function() {
   console.log("Doing a pomadoro");
   startTimer(25);
+  update_display();
 }
 
 var breaktime = function() {
@@ -28,5 +30,11 @@ var breaktime = function() {
 var stop = function() {
   clearTimeout(timer);
   console.log("Timer has been stopped");
+  stop_count++;
+  update_display();
 }
 
+var update_display = function() {
+  document.getElementById("status").innerHTML = "<strong>Pomadoros</strong> " + pomadoro_count + 
+                                                "  <strong>Stops</strong> " + stop_count;
+}
